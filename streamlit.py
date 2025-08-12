e89b77b4a96723a3f9132d2c7570dd8510c585fc
rayden <rayden.ai@gmail.com>
2025-08-12 14:40:29 +0100

    fix key issues

--- streamlit.py
+++ streamlit.py
@@ -5,10 +5,15 @@ import asyncio
 from dotenv import load_dotenv
 from utils import extract_video_id, get_transcript, generate_social_media_post, get_available_models, format_transcript
 
-# Load API Key
-load_dotenv()
-key = os.getenv("GEMINI_API_KEY")
-st.session_state.gemini_api_key = key
+# Load API Key uncomment to use locally
+# load_dotenv()
+# key = os.getenv("GEMINI_API_KEY")
+# st.session_state.gemini_api_key = key
+# gemini_api_key = st.session_state.gemini_api_key
+
+#comment this out when using locally
+gem_api_key = st.secrets["GEMINI_API_KEY"]
+st.session_state.gemini_api_key = gem_api_key
 gemini_api_key = st.session_state.gemini_api_key
 
 # Set page config
